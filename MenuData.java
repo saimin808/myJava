80ec33bdb87a38336157f2fd30f5767b64a9506e
saimin808 <67626942+saimin808@users.noreply.github.com>
2020-09-20 22:09:06 +0900

    Add files via upload

---
+++ MenuData.java
@@ -0,0 +1,58 @@
+package kr.co.softsoldesk.beans;
+
+public class MenuData {
+	
+	private String mdate;
+	private String meal;
+	private String soup;
+	private String sub1;
+	private String sub2;
+	private String sub3;
+	private String kcal;
+	
+	public String getMdate() {
+		return mdate;
+	}
+	public void setMdate(String mdate) {
+		this.mdate = mdate;
+	}
+	public String getMeal() {
+		return meal;
+	}
+	public void setMeal(String meal) {
+		this.meal = meal;
+	}
+	public String getSoup() {
+		return soup;
+	}
+	public void setSoup(String soup) {
+		this.soup = soup;
+	}
+	public String getSub1() {
+		return sub1;
+	}
+	public void setSub1(String sub1) {
+		this.sub1 = sub1;
+	}
+	public String getSub2() {
+		return sub2;
+	}
+	public void setSub2(String sub2) {
+		this.sub2 = sub2;
+	}
+	public String getSub3() {
+		return sub3;
+	}
+	public void setSub3(String sub3) {
+		this.sub3 = sub3;
+	}
+	public String getKcal() {
+		return kcal;
+	}
+	public void setKcal(String kcal) {
+		this.kcal = kcal;
+	}
+	
+	
+
+}
